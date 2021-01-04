e3484a454f9baabb5924738c267d7d455e1d55fe
Natharam <201651033@iiitvadodara.ac.in>
2021-01-05 00:33:13 +0530

    Init

---
+++ src/component/Table/Table.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './table.css';
+
+function Table() {
+  return (
+    <div className="table">
+      <table>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        
+      </table>
+    </div>
+  );
+}
+
+export default Table;
---
+++ src/component/Table/tableData.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function tableData() {
+  return (
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>Smith</td>
+      <td>50</td>
+    </tr>
+  );
+}
+
+export default tableData;
